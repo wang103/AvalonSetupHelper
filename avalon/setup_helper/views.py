13aa868f8b768346306d8f212ab3dbe03eb862dd
Tianyi Wang <tianyiw@twitter.com>
2016-08-17 16:22:08 -0700

    init work

--- avalon/setup_helper/views.py
+++ avalon/setup_helper/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Home page, lists all rooms in the READY_TO_JOIN state.
+def index(request):
+  return HttpResponse("Hello, world")
+
