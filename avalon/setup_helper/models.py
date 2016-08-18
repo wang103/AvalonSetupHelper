9d69a6dee00f2df54ed483216b41761137050c43
Tianyi Wang <tianyiw@twitter.com>
2016-08-18 12:14:53 -0700

    Minor

--- avalon/setup_helper/models.py
+++ avalon/setup_helper/models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class Room(models.Model):
   name = models.CharField(max_length=100)
   num_players = models.IntegerField()
@@ -9,10 +11,17 @@ class Room(models.Model):
   created_at_date = models.DateTimeField()
   updated_at_date = models.DateTimeField()
 
+  def __str__(self):
+    return self.name
+
+@python_2_unicode_compatible
 class Player(models.Model):
   room = models.ForeignKey(Room, on_delete=models.CASCADE)
   name = models.CharField(max_length=100)
   token = models.CharField(max_length=10)
   character = models.IntegerField()
   created_at_date = models.DateTimeField()
+
+  def __str__(self):
+    return self.name
 
