a1cf040df02f3e97584f9e8694b917bf7f390637
Tianyi Wang <tianyiw@twitter.com>
2016-08-18 16:58:57 -0700

    Add error page

--- avalon/avalon/settings.py
+++ avalon/avalon/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 't8fg)85f2j_5z==ow7)t#4$x%fh76vhgdepw7p%!par=q35(4c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  '*',
+]
 
 
 # Application definition
--- avalon/setup_helper/views.py
+++ avalon/setup_helper/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
@@ -32,7 +33,35 @@ def room_info_before_join(request, room_id):
 
 # An action to join a room.
 def join_room(request, room_id):
-  return HttpResponse("stub")
+  try:
+    with transaction.atomic():
+      room = Room.objects.select_for_update().get(pk=room_id)
+
+      passcode = request.POST['passcode']
+      player_name = request.POST['player_name']
+
+      error_message = ""
+
+      # Check for passcode
+      if passcode != room.passcode:
+        error_message = "Wrong passcode"
+
+      # Check of player name
+      if not player_name:
+        error_message = "Invalid player name"
+
+      # Check for max number of players
+      existing_players = room.player_set.all()
+      if existing_players.count() >= room.num_players:
+        error_message = "Room is full"
+
+      if error_message != "":
+        return render(request, 'setup_helper/error.html', { 'error_message': error_message })
+
+
+
+  except Room.DoesNotExist:
+    raise Http404("Room does not exist")
 
 
 # The page player sees after player joins the room.
