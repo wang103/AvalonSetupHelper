a412ccee3a3d095f4394389cd9ae9277429f2236
Tianyi Wang <tianyiw@twitter.com>
2016-08-18 13:50:54 -0700

    Added urls for views

--- avalon/setup_helper/urls.py
+++ avalon/setup_helper/urls.py
@@ -4,4 +4,16 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+
+  url(r'^(?P<room_id>[0-9]+)/$', views.room_info_before_join, name='room_info_before_join'),
+
+  url(r'^(?P<room_id>[0-9]+)/player/(?<player_token>.+)/$', views.room_info_after_join, name='room_info_after_join'),
+
+  url(r'^(?P<room_id>[0-9]+)/join_room/$', views.join_room, name='join_room'),
+
+
+  url(r'^host/$', views.host, name='host'),
+
+  url(r'^create_room/$', views.create_room, name='create_room'),
 ]
+
