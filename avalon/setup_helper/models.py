0e5aad428cf5f67803c5f28f3e3467a43a0f7a2d
Tianyi Wang <tianyiw@twitter.com>
2016-08-17 23:17:31 -0700

    Add some init models

--- avalon/setup_helper/models.py
+++ avalon/setup_helper/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+  name = models.CharField(max_length=100)
+  num_players = models.IntegerField(min_value=5, max_value=10)
+  state = models.IntegerField()
+  created_at_date = models.DateTimeField()
+  updated_at_date = models.DateTimeField()
+
+class Player(models.Model):
+  room = models.ForeignKey(Room, on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  token = models.CharField(max_length=10)
+  character = models.IntegerField()
+  created_at_date = models.DateTimeField()
+
