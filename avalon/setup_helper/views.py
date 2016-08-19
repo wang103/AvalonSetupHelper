4898b78ad6857e8ca808cf79a2ebe528455bdf92
Tianyi Wang <tianyiw@twitter.com>
2016-08-18 18:35:34 -0700

    minor fix

--- avalon/setup_helper/views.py
+++ avalon/setup_helper/views.py
@@ -1,5 +1,5 @@
 from django.db import transaction
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils import timezone
