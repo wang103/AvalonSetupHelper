6ae8ef4dab37d4ac06472005bc79e889d7b9456c
Tianyi Wang <tianyiw@twitter.com>
2016-08-21 20:53:18 -0700

    More separate settings

--- avalon/avalon/settings/base.py
+++ avalon/avalon/settings/base.py
@@ -57,26 +57,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'avalon.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'HOST': 'localhost',
-    'PORT': '',           # empty for default
-    'NAME': 'avalon',     # DB name
-    'USER': 'saber',      # just a local user
-    'PASSWORD': 'saber',  # just a local user
-  },
-  'sqlite3': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -108,10 +88,4 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
 
--- avalon/avalon/settings/local.py
+++ avalon/avalon/settings/local.py
@@ -10,3 +10,26 @@ ALLOWED_HOSTS = [
   '*',
 ]
 
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'HOST': 'localhost',
+    'PORT': '',           # empty for default
+    'NAME': 'avalon',     # DB name
+    'USER': 'saber',      # just a local user
+    'PASSWORD': 'saber',  # just a local user
+  },
+  'sqlite3': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
+
