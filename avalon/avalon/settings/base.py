2075311514c75d5028208db9ba88634a0cf32d6c
Tianyi Wang <tianyiw@twitter.com>
2016-08-21 20:13:44 -0700

    Start separating config

--- avalon/avalon/settings/base.py
+++ avalon/avalon/settings/base.py
@@ -15,21 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't8fg)85f2j_5z==ow7)t#4$x%fh76vhgdepw7p%!par=q35(4c'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-  '*',
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
--- avalon/avalon/settings/local.py
+++ avalon/avalon/settings/local.py
@@ -1,2 +1,12 @@
 from avalon.settings.base import *
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 't8fg)85f2j_5z==ow7)t#4$x%fh76vhgdepw7p%!par=q35(4c'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+  '*',
+]
+
